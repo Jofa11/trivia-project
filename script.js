64395bdf31951ec775a18a4fbc9e3049367968ff
Joshua Favre <joshuaf@MacBook-Pro.local>
2020-07-10 13:33:18 -0500

    linked pages psuedocoded js

---
+++ script.js
@@ -0,0 +1,11 @@
+// function for play button
+
+// function for scoreboard display and update
+
+// function for game messages to display
+
+// function to make questions to appear
+
+// function for answer buttons
+
+// function function to end game
