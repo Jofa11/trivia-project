9c8950b7e725e55a86cabf10c145747e2785d711
Jofa11 <favrejoshua@yahoo.com>
2020-07-11 11:57:51 -0500

    atart button changes for initial to game screen

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+const initialPage = document.querySelector('.initial-page');
+const gamePage = document.querySelector('.game-page');
 // function for play button
 const playBtn = document.querySelector('#play');
 playBtn.addEventListener('click', handlePlayBtn);
 function handlePlayBtn() {
-	console.log('game time');
+    initialPage.classList.add('hidden');
+    gamePage.classList.remove('hidden');
 }
 
 // function for scoreboard display and update
 
 // function for game messages to display
 
-// function to make questions to appear
+// function to make questions appear
 
 // function for answer buttons
 const answerBtns = document.querySelectorAll('#btn-div');
 answerBtns.forEach((btn) => {
-    btn.addEventListener('click', handleAnswerBtn);
+	btn.addEventListener('click', handleAnswerBtn);
 });
 function handleAnswerBtn() {
-    console.log('correct');
+	console.log('correct');
 }
 
-    
-
-// function function to end game
+// function to end game
